60b71624b40760ab1604007c2f1cdfb67f1cb1c0
Miriam Blanco <miriam.blanco.lm@gmail.com>
2024-09-20 12:55:32 +0200

    modificaciones pass y frontend_port

--- variables.tf
+++ variables.tf
@@ -37,13 +37,13 @@ variable "vm_map" {
       name           = "app-server-1"
       size           = "Standard_DS1_v2"
       admin_user     = "adminuser1"
-      admin_password = "adminuser1"
+      admin_password = "Admin-user1"
     },
     "vm2" = {
       name           = "app-server-2"
       size           = "Standard_DS1_v2"
       admin_user     = "adminuser2"
-      admin_password = "adminuser2"
+      admin_password = "Admin-user2"
     }
   }
 }
