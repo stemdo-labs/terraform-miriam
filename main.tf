3e59c682486758dc891632e428b83480c8ec1f18
Miriam Blanco <miriam.blanco.lm@gmail.com>
2024-09-24 12:08:37 +0200

    provider

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 # Data source to reference an existing resource group
