037318303dc2b7141d354adf46a81f6719d2fcec
Miriam Blanco <miriam.blanco.lm@gmail.com>
2024-09-25 15:44:10 +0200

    modificacion outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-# IP pública del balanceador de carga
-output "lb_public_ip" {
-  description = "La dirección IP pública del balanceador de carga"
-  value       = module.load_balancer.lb_public_ip
-}
+# # IP pública del balanceador de carga
+# output "lb_public_ip" {
+#   description = "La dirección IP pública del balanceador de carga"
+#   value       = module.load_balancer.lb_public_ip
+# }
 
-# IDs de las máquinas virtuales
-output "vm_ids" {
-  description = "IDs de las máquinas virtuales creadas"
-  value       = module.virtual_machines.vm_ids
-}
+# # IDs de las máquinas virtuales
+# output "vm_ids" {
+#   description = "IDs de las máquinas virtuales creadas"
+#   value       = module.virtual_machines.vm_ids
+# }
